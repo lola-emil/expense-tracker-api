0ba8acb75a2bc803db4dbf1da9484f5cdef82b33
lola-emil <staleexam19@gmail.com>
2024-03-05 12:30:17 +0800

    Added register route

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -5,7 +5,7 @@ import authGuard from "../../middlewares/authGuard";
 
 const router = Router();
 
-// router.post("/register", asyncHander(AuthController.register));
+router.post("/register", asyncHandler(AuthController.register));
 router.post("/login", asyncHandler(AuthController.login));
 router.post("/update-password", authGuard, asyncHandler(AuthController.updatePassword))
 export default router;
