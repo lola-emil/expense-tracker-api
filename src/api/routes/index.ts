1f5546d955c2873f252cdf4cf590847ea51142ef
Rens Saladaga <staleexam19@gmail.com>
2024-05-22 10:53:51 +0800

    Added update user password

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -4,6 +4,8 @@ import expenseRoute from "./expense";
 import authGuard from "../../middlewares/authGuard";
 import recordRoute from "./record";
 
+import userRoute from "./user";
+
 import { ErrorResponse } from "../../utils/response-util";
 
 const router = Router();
@@ -12,5 +14,7 @@ router.use("/auth", authRoute);
 router.use("/expense", authGuard, expenseRoute);
 
 router.use("/records", authGuard, recordRoute);
+
+router.use("/user", authGuard, userRoute);
 
 export default router;
---
+++ src/api/routes/user.ts
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import Joi from "joi";
+import { ApiResponse, ErrorResponse, handleResponse } from "../../utils/response-util";
+import * as UserRepo from "../../dal/user";
+import bcrypt from "bcrypt";
+import asyncHandler from "../../middlewares/asyncHandler";
+
+const router = Router();
+
+const updatePasswordSchema = Joi.object({
+    currentPassword: Joi.string().required(),
+    newPassword: Joi.string().required(),
+    confirmPassword: Joi.string().required()
+})
+
+router.post("/update-password", asyncHandler(async (req, res) => {
+    const userId = res.locals.userId
+    const { currentPassword, newPassword, confirmPassword } = req.body;
+
+    const { error } = updatePasswordSchema.validate({ currentPassword, newPassword, confirmPassword })
+
+    if (error) throw new ErrorResponse(400, error.message);
+    const matchedUser = await UserRepo.findById(userId);
+
+    // validate current password
+    if (!(await bcrypt.compare(currentPassword, matchedUser.password)))
+        throw new ErrorResponse(400, "Current password is incorrect");
+
+    // validate new and password confirmation
+    if (newPassword != confirmPassword)
+        throw new ErrorResponse(400, "Password doesn't match");
+
+    await UserRepo.updatePasswordById(userId, newPassword);
+
+    return handleResponse(new ApiResponse(200, 'Password updated'), res);
+}));
+
+export default router;
